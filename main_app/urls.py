a204d0fe4728daa74c9c22ec269f3bc4358287ff
Denise Onukwue <donukwue1@gmail.com>
2021-05-30 13:55:08 -0400

    updated code and folders

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('add/', views.add, name="add"),
-   
+    path('', views.items_index, name='index'),
+    path('add/', views.ItemCreate.as_view(), name='add'),
+    path('<int:item_id>/', views.items_detail, name='index'),
+    path('<int:pk>/delete/', views.ItemDelete.as_view(), name='delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,22 +1,37 @@
-from main_app.models import Item
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
-
-
+from django.http import HttpResponseRedirect
+from django.views.generic.edit import CreateView, DeleteView
+from .models import Item
+from .forms import ItemForm
 # Create your views here.
-class ItemCreate(CreateView):
-    model = Item
-    fields  = '__all__'
 
-def home(request):
-   return render(request, 'home.html')
-
-def add(request):
-    return render(request,'add.html' )
 
 def items_index(request):
     items = Item.objects.all()
-    return render(request, 'items_index.html', {'items': items})
+    return render(request, 'index.html', { 'items' : items })
+
+class ItemCreate(CreateView):
+    model = Item
+    fields = '__all__'
+    success_url = '/'
+
+
+def items_detail(request, item_id):
+    item = Item.objects.get(id=item_id)
+    return render(request, 'index.html', {'item' : item })
+    
+
+class ItemDelete(DeleteView):
+  model = Item
+  success_url = '/'
+
+def showform(request):
+    form = ItemForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        context = {'form': form }
+    return render(request, 'index.html', context)
+
 
 
 
